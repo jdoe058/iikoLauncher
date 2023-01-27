22fbbeef48310faafd5ef21e858f4c0a5e55ca33
Zheka Doe <jdoe058@gmail.com>
2023-01-27 18:29:50 +0300

    Тестирование модели представления главного окна

---
+++ ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,23 @@
+﻿using iikoLauncher.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iikoLauncher.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        #region Заголовок окна
+        private string _Title = "iiko Launcher";
+
+        /// <summary>Заголовок окна : string</summary>
+        public string Title 
+        {
+            get => _Title;
+            set => Set(ref _Title, value);
+        }
+        #endregion
+    }
+}
