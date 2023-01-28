cd48b4bbfb7668762322b9dc333ad635cc19605e
Zheka Doe <jdoe058@gmail.com>
2023-01-28 08:04:18 +0300

    добавлено свойство статус модели представления главного окна

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -9,15 +9,34 @@ namespace iikoLauncher.ViewModels
 {
     internal class MainWindowViewModel : ViewModel
     {
-        #region Заголовок окна
+        #region Title : string - Заголовок окна
+
+        /// <summary>Заголовок окна</summary>
         private string _Title = "iiko Launcher";
 
-        /// <summary>Заголовок окна : string</summary>
+        /// <summary>Заголовок окна</summary>
         public string Title 
         {
             get => _Title;
             set => Set(ref _Title, value);
         }
+
         #endregion
+
+        #region Status : string - Статус программы
+
+        /// <summary>Статус программы</summary>
+        private string _Status = "Готов";
+
+        /// <summary>Статус программы</summary>
+        public string Status
+        {
+            get => _Status;
+            set => Set(ref _Status, value);
+        }
+
+        #endregion
+
+
     }
 }
